0dc3d81cafa4ab13f96b59ba83faabd4479cc4b6
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-07 23:56:22 +0200

    shop_air_app

---
+++ lib/screens/bags_screen_gallery/bags_screen_gallery.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BagsGalleryScreen extends StatefulWidget {
+  const BagsGalleryScreen({super.key});
+
+  @override
+  State<BagsGalleryScreen> createState() => _BagsGalleryScreenState();
+}
+
+class _BagsGalleryScreenState extends State<BagsGalleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screens/shoes_screen_gallery/shoes_gallery_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoesGalleryScreen extends StatefulWidget {
+  const ShoesGalleryScreen({super.key});
+
+  @override
+  State<ShoesGalleryScreen> createState() => _ShoesGalleryScreenState();
+}
+
+class _ShoesGalleryScreenState extends State<ShoesGalleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
