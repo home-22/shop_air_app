a9b5fb0bea4c3f139df1bcf5d576fc318723111f
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-09 21:37:54 +0200

    new dummy data

--- lib/data/dummy_data_list.dart
+++ lib/data/dummy_data_list.dart
@@ -27,10 +27,74 @@ List<Product> dummyData = [
   ),
   Product(
     id: '45hg546h657jn467j4',
-    name: 'Smart 8k',
+    name: 'Samsung Led 60 4K Ultra HD',
     description:
         'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
     price: '644.00',
     image: 'assets/images/electronics3.jpg',
+  ),
+  Product(
+    id: '45hg546h657jn467j5',
+    name: 'Tablet Holder OX321M',
+    description:
+        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+    price: '14.00',
+    image: 'assets/images/electronics4.jpg',
+  ),
+  Product(
+    id: '45hg546h657jn467j6',
+    name: 'XO USB wall charger L59 2.4A + Lighting cable ',
+    description:
+        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+    price: '20.00',
+    image: 'assets/images/electronics5.jpg',
+  ),
+  Product(
+    id: '45hg546h657jn467j7',
+    name: 'XO NB105 Cable 2m',
+    description:
+        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+    price: '11.00',
+    image: 'assets/images/electronics6.jpg',
+  ),
+  Product(
+    id: '45hg546h657jn467j8',
+    name: 'XO Geming RGB GE04',
+    description:
+        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+    price: '38.00',
+    image: 'assets/images/electronics7.jpg',
+  ),
+  Product(
+    id: '45hg546h657jn467j9',
+    name: 'Smart 24 mod',
+    description:
+        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+    price: '64.50',
+    image: 'assets/images/electronics8.jpg',
+  ),
+  Product(
+    id: '45hg546h657jn467j10',
+    name: 'Logitech Wirelless Mouse Black',
+    description:
+        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+    price: '43.00',
+    image: 'assets/images/electronics10.jpg',
+  ),
+  Product(
+    id: '45hg546h657jn467j11',
+    name: 'K-120 Black',
+    description:
+        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+    price: '54.50',
+    image: 'assets/images/electronics11.jpg',
+  ),
+  Product(
+    id: '45hg546h657jn467j12',
+    name: 'Nintendo Switch WiFi Controller White',
+    description:
+        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+    price: '78.50',
+    image: 'assets/images/electronics12.jpg',
   ),
 ];
