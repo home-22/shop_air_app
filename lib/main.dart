0f97ff89b02e37a1057860cff257272bf64b1188
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-10 10:27:06 +0200

    editing main

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_air_app/screens/welcome_screen/welcome_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main()
+//async
+{
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
