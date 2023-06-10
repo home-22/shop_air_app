bb4c0f334ef72e09165d51ed05373436ab56fa68
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-10 10:23:08 +0200

    editing home screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_air_app/colors/app_colors.dart';
-import 'package:shop_air_app/screens/bags_screen_gallery/bags_screen_gallery.dart';
-import 'package:shop_air_app/screens/home_screen/components/range_card.dart';
-import 'package:shop_air_app/screens/men_screen_gallery/men_gallery_screen.dart';
-import 'package:shop_air_app/screens/shoes_screen_gallery/shoes_gallery_screen.dart';
-import 'package:shop_air_app/screens/women_screen.gallery/women_screen_gallery.dart';
+import 'package:shop_air_app/data/dummy_data_list.dart';
+import 'package:shop_air_app/screens/product_detail_screen/product_detail_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -24,28 +21,60 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           elevation: 0,
           title: Text(
-            'Welcome to Shop',
+            'Awesome Store',
             style: TextStyle(fontSize: 24, color: AppColors.kWhite),
           ),
-          bottom: TabBar(
-            isScrollable: true,
-            indicatorColor: AppColors.kRed,
-            indicatorWeight: 5,
-            tabs: const [
-              RangeCard(title: 'Men'),
-              RangeCard(title: 'Women'),
-              RangeCard(title: 'Shoes'),
-              RangeCard(title: 'Bags')
-            ],
-          ),
         ),
-        body: const TabBarView(
-          children: [
-            MenGalleryScreen(),
-            WomenGaleryScreen(),
-            ShoesGalleryScreen(),
-            BagsGalleryScreen(),
-          ],
+        body: GridView.builder(
+          itemCount: dummyData.length,
+          itemBuilder: (context, i) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        ProductDetaliScreen(id: dummyData[i].id),
+                  ),
+                );
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.45,
+                height: MediaQuery.of(context).size.height * 0.35,
+                decoration: BoxDecoration(
+                  color: AppColors.kBluee,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 4,
+                      color: AppColors.kAmber,
+                      offset: const Offset(0, 1),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Image.asset(dummyData[i].image),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(dummyData[i].name),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(dummyData[i].price),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, childAspectRatio: 1),
         ),
         backgroundColor: AppColors.kWhite,
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
