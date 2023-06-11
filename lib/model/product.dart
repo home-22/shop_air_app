5a478e6dbcf5ca46f02b347c2e17f449b3ed0bfa
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-11 21:10:51 +0200

    edit_product

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,9 +1,10 @@
 class Product {
-  String id;
-  String name;
-  String description;
-  String price;
-  String image;
+  final String id;
+  final String name;
+  final String description;
+  final String price;
+  final String image;
+  final bool isAssetImage;
 
   Product({
     required this.id,
@@ -11,5 +12,11 @@ class Product {
     required this.description,
     required this.price,
     required this.image,
+    required this.isAssetImage,
   });
 }
+
+List<Product> productList = [];
+// productList je prazna list objekta tipa Product
+// mozemo pohraniti više instanci  klase Product
+//  dadajemo  tako pomocu metode add productList.add(product1 );
