cadfabe910ea587fcafc523c7d1f087773e213a4
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-12 19:21:28 +0200

    add screnn  edit_product_screen

--- lib/screens/add_product_screen/add_product_screen.dart
+++ lib/screens/add_product_screen/add_product_screen.dart
@@ -3,13 +3,19 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:shop_air_app/colors/app_colors.dart';
 
 import 'package:shop_air_app/model/product.dart';
 import 'package:image_picker/image_picker.dart';
 
 class AddProductScreen extends StatefulWidget {
+  final Product? product;
   final List<Product> productList;
-  const AddProductScreen({super.key, required this.productList});
+  const AddProductScreen({
+    super.key,
+    this.product,
+    required this.productList,
+  });
 
   @override
   State<AddProductScreen> createState() => _AddProductScreenState();
@@ -21,112 +27,182 @@ class _AddProductScreenState extends State<AddProductScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController descController = TextEditingController();
   String? selectedImage;
+  bool imageSelected = false;
+// _selectImage() za odabir slike iz galerije uređaja
   Future<void> _seletImage() async {
+// zatim se poziva metoda pickImage() nad imagePicker objektom i prosljeđuje se
+// ImageSource.gallery kao izvor slike Ovo znaci da cemo odbrati sliku iz galerije uređaja
+// motoda pickImage je asinkrona zatim koristimo await da bih smo pričekali da se odabir slike dovrši
+
     final imagePicker = ImagePicker();
     final pickedImage =
         await imagePicker.pickImage(source: ImageSource.gallery);
-
+// uslov ako je slika uspješno odabrana  pickedImage nije null
+// tada ažuriramo stanje widgeta koristeci setState()
+// i postavljamo selectedImage = na putanju odabira slike (pickedImage.path)
+// odabrana slika se pohranjuje u (selectedImage)varijablu što ce se odraziti na prikaz slike u slučaju osvježenja aplikacije
     if (pickedImage != null) {
       setState(() {
         selectedImage = pickedImage.path;
+        imageSelected = true;
       });
     }
   }
 
   @override
+  void initState() {
+    super.initState();
+    if (widget.product != null) {
+      priceController.text = widget.product!.price;
+      nameController.text = widget.product!.name;
+      descController.text = widget.product!.description;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Product to List..'),
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Form(
-            key: form,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.90,
-                    child: TextFormField(
-                      controller: priceController,
-                      decoration: const InputDecoration(
-                        labelText: 'price',
-                        hintText: 'price.. \$',
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'please enter price';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.90,
-                    child: TextFormField(
-                      controller: nameController,
-                      decoration: const InputDecoration(
-                        labelText: 'product name',
-                        hintText: 'Enter product name ',
+    return GestureDetector(
+      onTap: () {
+        // da omogućilo da se tastatura sakrije kada korisnik dodirne bilo koji dio ekrana izvan polja za unos teksta
+        // na taj nacin se omogućava korisniku da se zatvori tastaturu i završi sa uređivanje teksta
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: AppColors.kRed,
+          title: const Text('Add Product to List..'),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Form(
+              key: form,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.90,
+                      child: TextFormField(
+                        controller: priceController,
+                        decoration: const InputDecoration(
+                          labelText: 'price',
+                          hintText: 'price.. \$',
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'please enter price';
+                          }
+                          return null;
+                        },
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.90,
-                    child: TextFormField(
-                      controller: descController,
-                      decoration: const InputDecoration(
-                        labelText: 'product description',
-                        hintText: 'Enter product description',
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.90,
+                      child: TextFormField(
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          labelText: 'product name',
+                          hintText: 'Enter product name ',
+                        ),
                       ),
                     ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    _seletImage();
-                  },
-                  icon: const Icon(
-                    Icons.add_a_photo,
-                    size: 35,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.90,
+                      child: TextFormField(
+                        controller: descController,
+                        decoration: const InputDecoration(
+                          labelText: 'product description',
+                          hintText: 'Enter product description',
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                if (selectedImage != null) Image.file(File(selectedImage!)),
-              ],
+                  IconButton(
+                    onPressed: () {
+                      _seletImage();
+                    },
+                    icon: const Icon(
+                      Icons.add_a_photo,
+                      size: 35,
+                    ),
+                  ),
+                  // ako selectedImage je razlicit od null
+                  // ovo osigurava da ce se slika prikazati ako postoji odabrana slika
+                  if (selectedImage != null)
+
+                    // Image.file(File(selectedImage!)) kreira (Image) widget koji prikazuje sliku
+                    // na osnovu putanje selctedImage File(selectedImage!) i konvertuje putanju selcetedImage u File objekat
+                    // kako bih se mogla prikazati slika(za prikazivanje slike iz galerije ili kamere uređaja)
+
+                    Image.file(
+                      File(selectedImage!),
+                    ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.save),
-        onPressed: () {
-          if (form.currentState!.validate()) {
-            Product product = Product(
-                id: DateTime.now().toString(),
-                name: nameController.text,
-                description: descController.text,
-                price: priceController.text,
-                image: selectedImage!,
-                isAssetImage: false);
-            setState(() {
-              widget.productList.add(product);
-            });
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.save),
+            onPressed: () {
+              if (form.currentState!.validate()) {
+                //  ako je widget.product ima vrijednost  tada uređujemo postojući prizvod.
+                // Azuriramo  prizvod sa unesenim novim vrijednostima i zamjenujemo postojeci  u productListi sa ažuriranim prizvodiom
+                // ako je widget product ima vrijednost null tada ce dodati novi prizvod na listu
+                if (widget.product == null) {
+                  // Dodajemo novi prizvod na listu
+                  Product newProduct = Product(
+                    id: UniqueKey().toString(),
+                    name: nameController.text,
+                    description: descController.text,
+                    price: priceController.text,
+                    image: selectedImage!,
+                    isAssetImage: false,
+                  );
+                  setState(() {
+                    widget.productList.add(newProduct);
+                  });
+                } else {
+                  // ovdje ažuriramo  postojeći prizvod na listi
+                  Product updateProduct = Product(
+                    id: widget.product!.id,
+                    name: nameController.text,
+                    description: descController.text,
+                    price: priceController.text,
+                    image: selectedImage ?? widget.product!.image,
+                    isAssetImage: widget.product!.isAssetImage,
+                  );
 
-// ocistiti kontrolere nakon sto spremimo
-            nameController.clear();
-            descController.clear();
-            priceController.clear();
-            Navigator.pop(context);
-          }
-        },
+                  // metoda indexWhere se poziva  na widgetom.productList tj primjenuje se
+                  // na svaki element u listi Ova metoda prima funkciju koja se izvršava za svaki element i provjerava određeni uslov
+                  // u ovom  slučaju uslov(uvjet) je id prizvoda jednak id trenutnog uređivanog prizvoda widget.product!.id
+                  final index = widget.productList.indexWhere(
+                      (product) => product.id == widget.product!.id);
+                  if (index != -1) {
+                    // obavještavamo da se stanje aplikacije mijenja ažuriramo nove podatke
+                    // tj na odgovarajući index widget.productList čime se ažurira postojeći prizvod
+
+                    setState(() {
+                      widget.productList[index] = updateProduct;
+                    });
+                  }
+                }
+
+                FocusScope.of(context).unfocus();
+                // ocistiti kontrolere nakon sto spremimo podatke
+                nameController.clear();
+                descController.clear();
+                priceController.clear();
+                Navigator.pop(context);
+              }
+            }),
       ),
     );
   }
