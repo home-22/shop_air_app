03c67a76c621289b2ae9fb6b84f56cf3f3f76440
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-18 12:38:56 +0200

    let's add a bottom navigation bar

---
+++ lib/screens/home_screen/components/bottom_navigator_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:shop_air_app/colors/app_colors.dart';
+import 'package:shop_air_app/screens/add_product_screen/add_product_screen.dart';
+
+class BottomNavigatorBar extends StatefulWidget {
+  const BottomNavigatorBar({super.key});
+
+  @override
+  State<BottomNavigatorBar> createState() => _BottomNavigatorBarState();
+}
+
+class _BottomNavigatorBarState extends State<BottomNavigatorBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(3.0),
+      height: 40,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: AppColors.kAmber.withOpacity(0.6),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(18),
+            topRight: Radius.circular(18),
+          ),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 5),
+              blurRadius: 20,
+              color: AppColors.kRed,
+            )
+          ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AddProductScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.category_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
