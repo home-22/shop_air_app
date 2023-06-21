41a57ed3ec25e77ab37568d35e035252359151fa
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-21 15:31:11 +0200

    let's add a dummy data category

--- lib/data/dummy_data_list.dart
+++ lib/data/dummy_data_list.dart
@@ -9,6 +9,7 @@ List<Product> dummyData = [
     price: '1150.00',
     image: 'assets/images/electronics0.jpg',
     isAssetImage: false,
+    category: Category.computers,
   ),
   Product(
     id: '45hg546h657jn467j2',
@@ -18,6 +19,7 @@ List<Product> dummyData = [
     price: '450.00',
     image: 'assets/images/electronics1.jpg',
     isAssetImage: false,
+    category: Category.laptops,
   ),
   Product(
     id: '45hg546h657jn467j3',
@@ -27,26 +29,26 @@ List<Product> dummyData = [
     price: '1220.00',
     image: 'assets/images/electronics2.jpg',
     isAssetImage: false,
+    category: Category.laptops,
   ),
   Product(
-    id: '45hg546h657jn467j4',
-    name: 'Samsung Led 60 4K Ultra HD',
-    description:
-        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
-    price: '644.00',
-    image: 'assets/images/electronics3.jpg',
-    isAssetImage: false,
-  ),
+      id: '45hg546h657jn467j4',
+      name: 'Samsung Led 60 4K Ultra HD',
+      description:
+          'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+      price: '644.00',
+      image: 'assets/images/electronics3.jpg',
+      isAssetImage: false,
+      category: Category.other),
   Product(
-    id: '45hg546h657jn467j5',
-    name: 'Tablet Holder OX321M',
-    description:
-        'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
-    price: '14.00',
-    image: 'assets/images/electronics4.jpg',
-    isAssetImage: false,
-    // type: ItemType.laptops,
-  ),
+      id: '45hg546h657jn467j5',
+      name: 'Tablet Holder OX321M',
+      description:
+          'Lorem ipsum dolor sit amet,consecte-\ntuer adipiscing elit,sed diam nonummy\nribh euismod tincidunt ut laoreet dolore\nmagna aliquam erat volutpat.',
+      price: '14.00',
+      image: 'assets/images/electronics4.jpg',
+      isAssetImage: false,
+      category: Category.other),
   Product(
     id: '45hg546h657jn467j6',
     name: 'XO USB wall charger L59 2.4A + Lighting cable ',
@@ -55,7 +57,7 @@ List<Product> dummyData = [
     price: '20.00',
     image: 'assets/images/electronics5.jpg',
     isAssetImage: false,
-    // type: ItemType.phones,
+    category: Category.phones,
   ),
   Product(
     id: '45hg546h657jn467j7',
@@ -65,6 +67,7 @@ List<Product> dummyData = [
     price: '11.00',
     image: 'assets/images/electronics6.jpg',
     isAssetImage: false,
+    category: Category.phones,
   ),
   Product(
     id: '45hg546h657jn467j8',
@@ -74,6 +77,7 @@ List<Product> dummyData = [
     price: '38.00',
     image: 'assets/images/electronics7.jpg',
     isAssetImage: false,
+    category: Category.laptops,
   ),
   Product(
     id: '45hg546h657jn467j9',
@@ -83,6 +87,7 @@ List<Product> dummyData = [
     price: '64.50',
     image: 'assets/images/electronics8.jpg',
     isAssetImage: false,
+    category: Category.phones,
   ),
   Product(
     id: '45hg546h657jn467j10',
@@ -92,6 +97,7 @@ List<Product> dummyData = [
     price: '43.00',
     image: 'assets/images/electronics10.jpg',
     isAssetImage: false,
+    category: Category.laptops,
   ),
   Product(
     id: '45hg546h657jn467j11',
@@ -101,6 +107,7 @@ List<Product> dummyData = [
     price: '54.50',
     image: 'assets/images/electronics11.jpg',
     isAssetImage: false,
+    category: Category.laptops,
   ),
   Product(
     id: '45hg546h657jn467j12',
@@ -110,5 +117,6 @@ List<Product> dummyData = [
     price: '78.50',
     image: 'assets/images/electronics12.jpg',
     isAssetImage: false,
+    category: Category.phones,
   ),
 ];
