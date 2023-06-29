dbd1ad73399af1b43892b48df78b5f870449d504
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-06-29 23:03:05 +0200

    bottom navigation bar changes

--- lib/screens/home_screen/components/bottom_navigator_bar.dart
+++ lib/screens/home_screen/components/bottom_navigator_bar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_air_app/colors/app_colors.dart';
+import 'package:shop_air_app/data/dummy_data_list.dart';
+import 'package:shop_air_app/providers/product_search_provider.dart';
 import 'package:shop_air_app/screens/add_product_screen/add_product_screen.dart';
 import 'package:shop_air_app/screens/cart_%20screen/cart_screen.dart';
 
@@ -11,57 +14,95 @@ class BottomNavigatorBar extends StatefulWidget {
 }
 
 class _BottomNavigatorBarState extends State<BottomNavigatorBar> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(3.0),
-      height: 40,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-          color: AppColors.kAmber.withOpacity(0.6),
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(18),
-            topRight: Radius.circular(18),
-          ),
-          boxShadow: [
-            BoxShadow(
-              offset: const Offset(0, 5),
-              blurRadius: 20,
-              color: AppColors.kRed,
-            )
-          ]),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AddProductScreen(),
-                ),
-              );
-            },
-            icon: const Icon(Icons.add),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const CartScreen(
-                    orderProducts: [],
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        padding: const EdgeInsets.all(3.0),
+        height: 60,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: AppColors.kWhite,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Consumer<ProductSearchProvider>(
+              builder: (context, productSearchProvider, _) {
+                return SizedBox(
+                  width: 50,
+                  child: IconButton(
+                    onPressed: () {
+                      productSearchProvider.searchProducts(dummyData);
+                    },
+                    icon: Icon(
+                      Icons.home_filled,
+                      color: AppColors.kBlue,
+                    ),
                   ),
+                );
+              },
+            ),
+            SizedBox(
+              width: 50,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const AddProductScreen(),
+                    ),
+                  );
+                },
+                icon: Icon(
+                  Icons.search,
+                  color: AppColors.kBlue,
                 ),
-              );
-            },
-            icon: const Icon(Icons.category_outlined),
-          ),
-        ],
+              ),
+            ),
+            SizedBox(
+              width: 50,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.shopping_cart,
+                  color: AppColors.kBlue,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 50,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CartScreen(
+                        orderProducts: [],
+                      ),
+                    ),
+                  );
+                },
+                icon: Icon(
+                  Icons.local_offer_outlined,
+                  color: AppColors.kBlue,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 50,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.person_rounded,
+                  color: AppColors.kBlue,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
