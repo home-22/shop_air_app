eddf8c412d3f55be6ef8eeee16ba6acf41bea24e
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-07-01 23:50:31 +0200

    change to product display tab

--- lib/screens/home_screen/components/product_display_tab.dart
+++ lib/screens/home_screen/components/product_display_tab.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 import 'package:shop_air_app/colors/app_colors.dart';
 import 'package:shop_air_app/data/dummy_data_list.dart';
 import 'package:shop_air_app/model/product.dart';
-import 'package:shop_air_app/providers/card_provider.dart';
+
 import 'package:shop_air_app/screens/product_detail_screen/product_detail_screen.dart';
 
 // Kartica za prikaz prizvoda iz liste
@@ -16,111 +17,105 @@ class ProductDisplayTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: SizedBox(
-        height: 220,
-        width: double.infinity,
-        child: ListView.builder(
-          itemCount: productList.length,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            final product = productList[index];
-            return GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => ProductDetaliScreen(
-                      id: product.id,
-                    ),
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 2,
+      width: double.infinity,
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 4,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: productList.length,
+        itemBuilder: (context, index) {
+          final product = productList[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => ProductDetaliScreen(
+                    id: product.id,
                   ),
-                );
-              },
-              child: Stack(
+                ),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(2.0),
+              ),
+              child: Column(
                 children: [
-                  Container(
-                    margin: const EdgeInsets.only(left: 10, top: 40),
-                    height: 210,
-                    width: 210,
-                    decoration: BoxDecoration(
-                      color: AppColors.kWhite,
-                      borderRadius: const BorderRadius.only(
-                        bottomRight: Radius.circular(30),
-                        topLeft: Radius.circular(30),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.kBluee,
-                          blurRadius: 25,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    right: 10,
-                    child:
-                        // Ako selectedImage nije null  jeste asset slika,
-                        // prikazujemo sliku koristeći Image.asset()
-
-                        CircleAvatar(
-                      maxRadius: 60,
-                      backgroundColor: AppColors.kAmber,
+                  Expanded(
+                    child: SizedBox(
+                      width: double.infinity,
                       child: product.isAssetImage
                           ? Image.asset(
                               product.image,
-                              filterQuality: FilterQuality.high,
-                              height: 100,
-                              width: 100,
+                              fit: BoxFit.cover,
                             )
                           : Image.file(
                               File(product.image),
-                              filterQuality: FilterQuality.high,
-                              height: 100,
-                              width: 100,
+                              fit: BoxFit.cover,
                               errorBuilder: (context, error, stackTrace) {
                                 return Image.asset(
                                   product.image,
-                                  height: 100,
-                                  width: 100,
+                                  fit: BoxFit.cover,
                                 );
                               },
                             ),
                     ),
                   ),
-                  Positioned(
-                    top: 110,
-                    left: 20,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(product.name),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Text(product.price),
-                        ),
-                      ],
+                  Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Text(
+                      product.name,
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.kBluee),
                     ),
                   ),
-                  Positioned(
-                    top: 50,
-                    left: 20,
-                    child: CircleAvatar(
-                      backgroundColor: AppColors.kRed,
-                      child: IconButton(
-                        onPressed: () {
-                          context.read<CardProvider>().removeItem(index);
-                        },
-                        icon: Icon(
-                          Icons.delete,
-                          color: AppColors.kWhite,
-                        ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      product.price,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.kBlue),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        child: RatingBar.builder(
+                            initialRating: 0,
+                            maxRating: 1,
+                            itemCount: 5,
+                            itemSize: 14,
+                            itemPadding: const EdgeInsets.symmetric(
+                              horizontal: 7.0,
+                              vertical: 6.0,
+                            ),
+                            allowHalfRating: true,
+                            itemBuilder: (context, _) {
+                              return Icon(
+                                Icons.star,
+                                color: AppColors.kAmber,
+                              );
+                            },
+                            onRatingUpdate: (rating) {
+                              print(rating);
+                            }),
                       ),
-                    ),
-                  ),
+                    ],
+                  )
                 ],
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
