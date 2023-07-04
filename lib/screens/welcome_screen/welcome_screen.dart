acadca5906e4b755569572a6567844781c7ceca2
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-07-04 21:11:23 +0200

    changes to the welcome screen

--- lib/screens/welcome_screen/welcome_screen.dart
+++ lib/screens/welcome_screen/welcome_screen.dart
@@ -1,64 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:shop_air_app/colors/app_colors.dart';
-import 'package:shop_air_app/screens/home_screen/home_screen.dart';
+import 'package:shop_air_app/screens/registar_screen/registar_screen.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const RegistarScreen(),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.kWhite,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/electronics13.jpg',
-              height: 300,
-              width: double.infinity,
-              filterQuality: FilterQuality.high,
-              fit: BoxFit.cover,
-            ),
-            Center(
-              child: ElevatedButton(
-                  style: TextButton.styleFrom(backgroundColor: AppColors.kBlue),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const HomeScreen(),
-                      ),
-                    );
-                  },
-                  child: const Text('Get started')),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 50),
-              child: Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Already have an account?',
-                      style: TextStyle(
-                        color: AppColors.kGrey,
-                        fontFamily: 'Helvetica-Bold',
-                        fontSize: 16,
-                      ),
-                    ),
-                    const TextSpan(
-                      text: 'Sing in ',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Helvetica-Bold',
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
+      backgroundColor: AppColors.kBlue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: AppColors.kWhite,
+                  width: 2.0,
                 ),
+                borderRadius: BorderRadius.circular(7.0),
+              ),
+              child: Image.asset(
+                'assets/images/electronics13.jpg',
+                height: 70,
+                width: 70,
+                fit: BoxFit.contain,
               ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20.0),
+          Text(
+            'Please wait',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w900,
+              color: AppColors.kWhite,
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          CircularProgressIndicator(
+            color: AppColors.kWhite,
+          ),
+        ],
       ),
     );
   }
