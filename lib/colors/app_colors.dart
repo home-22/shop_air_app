e8a2396160e30ff9e67ff8b862d22f4d97b1d15d
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-07-07 23:27:10 +0200

    changes to the app_colors

--- lib/colors/app_colors.dart
+++ lib/colors/app_colors.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // primarne boje
-  static Color kBlue = Color(0xFF40BFFF);
-  static Color kAmber = Color(0xFFFFC833);
-  static Color kRed = Color(0xFFFB7181);
-  static Color kPurple = Color(0xFF5C61F4);
-  static Color kGreen = Color(0xFF53D1B6);
-  // ostale boje
-  static Color kDark = Color(0xFF40BFFF);
-  static Color kLight = Color(0xFF0BFFF);
+  static Color kBlue = const Color(0xFF40BFFF);
+  static Color kAmber = const Color(0xFFFFC833);
+  static Color kRed = const Color(0xFFFB7181);
+  static Color kPurple = const Color(0xFF5C61F4);
+  static Color kGreen = const Color(0xFF53D1B6);
+
+  static Color kDark = const Color(0xFF091D46);
+  static Color kLight = const Color(0xFFFFFFD2);
   static Color kGrey = const Color(0xFFC4BDCA);
-  // bla colors
+
   static Color kWhite = const Color(0xFFFFFFFF);
 }
