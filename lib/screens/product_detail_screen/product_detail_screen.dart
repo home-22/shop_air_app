5c2aaaf49a273613f29d3aa3441f1a16f40bfb84
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-07-07 23:28:15 +0200

    changes to the product detail screen

--- lib/screens/product_detail_screen/product_detail_screen.dart
+++ lib/screens/product_detail_screen/product_detail_screen.dart
@@ -138,7 +138,7 @@ class ProductDetaliScreen extends StatelessWidget {
                 children: [
                   for (int i = 0; i < 5; i++)
                     Padding(
-                      padding: EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10.0),
                       child: Container(
                         height: 40,
                         width: 40,
