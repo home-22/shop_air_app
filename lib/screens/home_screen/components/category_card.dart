adb38b1fc7df4fd3d567ce37f57ff8a35a50ca5b
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-07-08 23:20:38 +0200

    adding a category card

---
+++ lib/screens/home_screen/components/category_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  String name;
+  Image image;
+  CategoryCard({
+    super.key,
+    required this.name,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: SizedBox(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              width: 50,
+              child: image,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: Text(name),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
