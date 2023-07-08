405b469a7a14ec3cf479b88d1af0d42a6417b75f
ArmiN JašareviĆ <armin.jasarevic.25@gmail.com>
2023-07-08 23:21:25 +0200

    changes to the home screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -7,6 +7,7 @@ import 'package:shop_air_app/data/dummy_data_list.dart';
 import 'package:shop_air_app/model/product.dart';
 
 import 'package:shop_air_app/screens/home_screen/components/bottom_navigator_bar.dart';
+import 'package:shop_air_app/screens/home_screen/components/category_card.dart';
 import 'package:shop_air_app/screens/home_screen/components/product_display_tab.dart';
 import 'package:shop_air_app/screens/home_screen/components/search_box.dart';
 import 'package:shop_air_app/screens/home_screen/components/super_flash_container.dart';
@@ -67,23 +68,42 @@ class _HomeScreenState extends State<HomeScreen> {
                       SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
                         child: Padding(
-                          padding: const EdgeInsets.all(5.0),
+                          padding: const EdgeInsets.all(2.0),
                           child: Row(
                             children: [
-                              for (int i = 0; i <= 4; i++)
-                                Padding(
-                                  padding: const EdgeInsets.all(5.0),
-                                  child: CircleAvatar(
-                                    radius: 30,
-                                    child: IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Icons.computer,
-                                        color: AppColors.kBlue,
-                                      ),
-                                    ),
+                              //  for (int i = 0; i <= 4; i++)
+                              CategoryCard(
+                                name: 'phones',
+                                image: const Image(
+                                  image: AssetImage(
+                                    'assets/images/electronics0.jpg',
                                   ),
                                 ),
+                              ),
+                              CategoryCard(
+                                name: 'laptops',
+                                image: const Image(
+                                  image: AssetImage(
+                                    'assets/images/electronics2.jpg',
+                                  ),
+                                ),
+                              ),
+                              CategoryCard(
+                                name: 'computers',
+                                image: const Image(
+                                  image: AssetImage(
+                                    'assets/images/electronics1.jpg',
+                                  ),
+                                ),
+                              ),
+                              CategoryCard(
+                                name: 'other',
+                                image: const Image(
+                                  image: AssetImage(
+                                    'assets/images/electronics9.jpg',
+                                  ),
+                                ),
+                              ),
                             ],
                           ),
                         ),
